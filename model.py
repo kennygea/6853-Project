b0f28f7f9b76cf859b33807da7df92e185ab55c3
Kenny <kennygea@gmail.com>
2017-05-16 21:56:13 -0400

    fixed?

--- model.py
+++ model.py
@@ -10,28 +10,7 @@ from six.moves import xrange
 from ops import *
 from utils import *
 
-def conv_out_size_same(size, stride):
-  return int(math.ceil(float(size) / float(stride)))
-
-class DCGAN(object):
-  def __init__(self, sess, input_height=108, input_width=108, is_crop=True,
-         batch_size=64, sample_num = 64, output_height=64, output_width=64,
-         y_dim=None, z_dim=100, gf_dim=64, df_dim=64,
-         gfc_dim=1024, dfc_dim=1024, c_dim=3, dataset_name='default',
-         input_fname_pattern='*.jpg', checkpoint_dir=None, sample_dir=None):
-    """
-
-    Args:
-      sess: TensorFlow session
-      batch_size: The size of batch. Should be specified before training.
-      y_dim: (optional) Dimension of dim for y. [None]
-      z_dim: (optional) Dimension of dim for Z. [100]
-      gf_dim: (optional) Dimension of gen filters in first conv layer. [64]
-      df_dim: (optional) Dimension of discrim filters in first conv layer. [64]
-      gfc_dim: (optional) Dimension of gen units for for fully connected layer. [1024]
-      dfc_dim: (optional) Dimension of discrim units for fully connected layer. [1024]
-      c_dim: (optional) Dimension of image color. For grayscale input, set to 1. [3]
-    """
+def conv_out_size_same(size, stride):   
     self.T = 5
     self.sess = sess
     self.is_crop = is_crop
@@ -132,7 +111,7 @@ class DCGAN(object):
         else:
           compare = tf.greater_equal(intermediate_weights, weights[i+1])
           intermediate_weights = tf.where(compare, intermediate_weights, weights[i+1])
-          self.G = tf.where(compare, self.G, weights[i+1])
+          self.G = tf.where(compare, self.G, G[i+1])
 
 
       # self.compare = tf.greater_equal(self.weight_one, self.weight_two)
@@ -209,7 +188,7 @@ class DCGAN(object):
         else:
           compare = tf.greater_equal(intermediate_weights, weights[i+1])
           intermediate_weights = tf.where(compare, intermediate_weights, weights[i+1])
-          self.G = tf.where(compare, self.G, weights[i+1])
+          self.G = tf.where(compare, self.G, G[i+1])
 
 
       # self.compare = tf.greater_equal(self.weight_one, self.weight_two)
