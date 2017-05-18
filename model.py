e526b37e6f56359a6e5af98f2662f91136304eb5
Kenny Gea <kennygea@gmail.com>
2017-05-17 23:47:28 -0400

    printing

--- model.py
+++ model.py
@@ -423,7 +423,7 @@ class DCGAN(object):
         print("Epoch: [%2d] [%4d/%4d] time: %4.4f, d_loss: %.8f, g_loss: %.8f" \
           % (epoch, idx, batch_idxs,
             time.time() - start_time, errD_fake+errD_real, errG))
-         weights = [i.eval({self.z: batch_z, self.y: batch_labels,self.h: h}) for i in self.weights_d]
+        weights = [i.eval({self.z: batch_z, self.y: batch_labels,self.h: h}) for i in self.weights_d]
         print(weights)
 
         if np.mod(counter, 100) == 1:
